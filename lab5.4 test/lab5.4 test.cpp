b401e53cb56620516d93d06b11b8176e2e5e65fa
milianets20 <marian.milianets.ri.2022@lpnu.ua>
2022-11-24 01:01:14 +0200

    fl

---
+++ lab5.4 test/lab5.4 test.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.04/Lab5.04.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab54test
+{
+	TEST_CLASS(lab54test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a;
+			a = S0(10, 20);
+			Assert::AreEqual(a, 0.466532, 0.0001);
+		}
+	};
+}
